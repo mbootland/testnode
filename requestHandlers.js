e42197288989dd2164ef4b5ddcbb0c8b8d115763
Boots88 <matthew.bootland@globalsign.com>
2017-07-04 14:32:39 +0900

    added formidable module and made show page show image

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,7 @@
 // var exec = require("child_process").exec;
 
-var querystring = require("querystring");
+var querystring = require("querystring")
+var fs = require("fs");
 
 function start(response, postData){
   console.log("Request handler 'start' was called");
@@ -39,5 +40,21 @@ function upload(response, postData){
   response.end();
 }
 
+function show(response, postData) {
+  console.log("Requests handler 'show' was called.");
+  fs.readFile("temp/test.gif", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
