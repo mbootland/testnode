1b6a31c9ac27b9c08aef4869eea47edd4d90ee10
Boots88 <matthew.bootland@globalsign.com>
2017-07-04 11:28:40 +0900

    server updated

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ function start(route, handle) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received");
 
-    route(handle, pathname);
+    route(handle, pathname, response);
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    var content = route(handle, pathname);
-    response.write(content);
-    response.end();
+    // response.writeHead(200, {"Content-Type": "text/plain"});
+    // var content = route(handle, pathname);
+    // response.write(content);
+    // response.end();
   }
 
   http.createServer(onRequest).listen(8888);
